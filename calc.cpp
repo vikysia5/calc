670d42a0d004ecaa4ee2204adefb557704479e89
vikysia5 <vika.ch10.2005@gmail.com>
2023-09-24 23:43:51 +0300

    first commit

---
+++ calc.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int a, b, op;   double result;
+  cout << "Введіть перше число\n";   cin >> a;
+  cout << "Введіть оператор\n1\t+\n2\t-\n3\t*\n4\t/\n\n";  cin >> op;
+  cout << "Введіть друге число\n";     cin >> b;
+  switch(op) {
+    case 1:     // code
+                    break;
+
+    case 2:    // code
+
+                    break;
+
+    case 3:     // code
+
+                    break;
+
+    case 4:     // code
+
+                    break;
+
+  }
+
+  cout << "Результат = " << result;
+
+}
