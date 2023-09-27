625ab8b1ea261300e89ad74036de2a2adcb98789
vikysia5 <vika.ch10.2005@gmail.com>
2023-09-27 09:46:56 +0300

    New feature added”

--- calc.cpp
+++ calc.cpp
@@ -3,24 +3,26 @@ using namespace std;
 int main() {
   int a, b, op;   double result;
   cout << "Введіть перше число\n";   cin >> a;
-  cout << "Введіть оператор\n1\t+\n2\t-\n3\t*\n4\t/\n\n";  cin >> op;
+  cout << "Введіть оператор\n1\t+\n2\t-\n3\t*\n4\t/\n5\t^\n";  cin >> op;
   cout << "Введіть друге число\n";     cin >> b;
   switch(op) {
-      case 1:     result = a + b;  break;
+      case 1:     result = a + b;  
                     break;
 
       case 2:    result = a - b;
 
                     break;
 
-    case 3:     // code
+      case 3:     result = a * b;
 
                     break;
 
       case 4:     result = (double) a / b;
 
                     break;
+      case 5:     result = pow(a,b);
 
+                    break;
   }
 
   cout << "Результат = " << result;
